a084015d4d81ec05abd3f152d3878b9026e0a669
Faye Cheng <fayehcheng@gmail.com>
2017-04-04 21:50:37 -0700

    TDD Exercise 1

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
+
     public Integer compute(int i) {
-        return -1;
+        int fact = 1;
+        for(int j = 1; j <= i; j++) {
+            fact *= j;
+        }
+        return fact;
     }
 }
