19ea3828dd1c0411df8ccfc2c332cc46cb6a6b75
Faye Cheng <fayehcheng@gmail.com>
2017-04-05 18:45:18 -0700

    TDD Exercise 2

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,30 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    private double accountBalance = 0;
+
+    public Account(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public Account() {
+        this.accountBalance = 0;
+    }
+
+    void deposit(double money) {
+        accountBalance += money;
+    }
+
+    void withdraw(double money) {
+        if(accountBalance - money < 0) {
+            System.out.println("Insufficient funds for withdrawl.");
+        } else {
+            accountBalance -= money;
+        }
+    }
+
+    double getAccountBalance() {
+        return accountBalance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,24 +1,32 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import com.thoughtworks.tddintro.exercises.factorial.Factorial;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
+        assertEquals(account.getAccountBalance(), 100, 0.001);
+        account.deposit(50);
+        assertEquals(account.getAccountBalance(), 150, 0.001);
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+        Account account = new Account(100);
+        account.withdraw(50);
+        assertEquals(account.getAccountBalance(), 50, 0.001);
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-
+        Account account = new Account(50);
+        account.withdraw(100);
+        assertEquals(account.getAccountBalance(), 50, 0.001);
     }
 }
